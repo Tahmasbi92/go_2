402516265d0a048bc29c879e5485c426f57e0247
tahmasbi92 <atemaaaa92@gmail.com>
2024-12-13 21:30:44 +0330

    اضافه کردن فایل

---
+++ B_15_a4.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	originalNumbers := []string{"2", "341", "5", "11", "13", "1", "12", "43"}
+	//                        0     1     2     3     4    5     6     7
+
+	for index_number, index_value := range originalNumbers {
+		fmt.Println(index_number, "=", index_value+"**")
+	}
+}
