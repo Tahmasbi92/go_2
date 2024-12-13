b6097f74443c85a36f5a3ac3abdcdd20a9087b42
tahmasbi92 <atemaaaa92@gmail.com>
2024-12-13 20:54:48 +0330

    اضافه کردن فایل

---
+++ B_14.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	for number := 0; number <= 3; number++ {
+		fmt.Println(number)
+	}
+	// for initiallization مقدار دهی; condition شرط; postcondition شرط بعد از اجرا
+	// do something
+	fmt.Println("The End")
+}
