c6317a7401b2630c7da472e958488f1de43b8286
tahmasbi92 <atemaaaa92@gmail.com>
2025-01-05 22:02:10 +0330

    اضافه کردن فایل

---
+++ N_2_3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myage int = 27
+	var myname string = "AlirezaNoorghorbani"
+	var mysurname string = "Noorghorbani"
+	var temp float64 = 26.7
+	//int => addad sahih
+	//string => jomle ya kalame
+	//float => addad ashari
+	var isprogrammer bool = true
+	fmt.Println(myname, myage, mysurname, temp, isprogrammer)
+}
