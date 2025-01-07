b12aa7e1541486e458aae9725a1142819b08c389
tahmasbi92 <atemaaaa92@gmail.com>
2025-01-07 20:38:51 +0330

    اضافه کردن فایل

---
+++ L_18_7.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	age := 35
+
+	if age >= 18 && age <= 35 {
+		fmt.Println("You are in 18 to 35.")
+	} else if age <= 0 {
+		fmt.Println("You are not born!!!")
+	} else if age > 0 && age < 18 {
+		fmt.Println("You are a young.")
+	} else {
+		fmt.Println("You are a old.")
+	}
+}
